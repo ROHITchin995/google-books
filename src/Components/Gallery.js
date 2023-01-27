1acd17e5d3ecbb6ce62bebeeddea97083e3cb0f1
ROHITchin995 <rohitsure.9915@gmail.com>
2023-01-27 21:44:22 +0530

    no image url issue fixed

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,15 +1,16 @@
 import { Component } from "react";
-import book from '../Images/771.jpg'
 import './Gallery.css'
+import book from '../Images/01.jpg';
 class Gallery extends Component{
     render(){
         return(
             <div>
                 {this.props.items.map((item, index)=>{
                     let {title, imageLinks, infoLink} = item.volumeInfo;
+                    console.log(title, imageLinks)
                     return(
                         <a href={infoLink} key={index} target="_blank" rel="noopener" className="book">
-                            <img src={undefined !== imageLinks ? imageLinks.thumbnail : {book}} 
+                            <img src={(imageLinks !== undefined) ? imageLinks.thumbnail : "https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Open_book_nae_02.svg/2560px-Open_book_nae_02.svg.png" } 
                             alt={`Pictured: The cover for the book ${title}`} 
                             className="bookCover"/>
                             <header className="bookTitle">
